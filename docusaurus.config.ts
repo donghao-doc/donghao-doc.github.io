dcdc5aebde11a14d474df8ee0a7f1475994c738d
BarryDong <donghao@bwcj.com>
2024-09-27 22:28:24 +0800

    暗黑模式代码主题 & 行内代码样式

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -129,7 +129,7 @@ const config: Config = {
     },
     prism: {
       theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      darkTheme: prismThemes.oceanicNext,
     },
     docs: {
       sidebar: {
