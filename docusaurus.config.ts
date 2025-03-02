c5ce84b905600af488e9e7ff13185ce015618534
BarryDong <donghao@bwcj.com>
2025-03-02 14:45:31 +0800

    创建 React navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -77,18 +77,10 @@ const config: Config = {
         src: 'img/logo.svg',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        { type: 'docSidebar', sidebarId: 'reactSidebar', position: 'left', label: 'React' },
+        // { type: 'docSidebar', sidebarId: 'tutorialSidebar', position: 'left', label: 'Tutorial' },
+        // { to: '/blog', label: 'Blog', position: 'left' },
+        { href: 'https://github.com/facebook/docusaurus', label: 'GitHub', position: 'right' },
       ],
     },
     footer: {
