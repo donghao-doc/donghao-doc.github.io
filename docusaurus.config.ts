8cacbfeb1a1db3e22c5a2f369eef3193f8b7d7c8
BarryDong <donghao@bwcj.com>
2025-03-09 17:58:36 +0800

    支持 scss 语法高亮

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['bash'],
+      additionalLanguages: ['bash', 'scss'],
     },
   } satisfies Preset.ThemeConfig,
 };
