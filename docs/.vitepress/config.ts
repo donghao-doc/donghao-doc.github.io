3c13356e9dea6499a8c7b020db7166b3dda75cf7
BarryDong <286084845@qq.com>
2025-08-31 13:18:03 +0800

    更新 GitHub 链接

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/donghao-doc/donghao-doc.github.io' }
     ]
   }
 })
