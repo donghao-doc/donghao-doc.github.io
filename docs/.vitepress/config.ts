9ca0a699d70f24f71a6de10519c41af91f993a2f
BarryDong <286084845@qq.com>
2025-08-31 17:24:05 +0800

    简洁的 URL

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Barry's Docs",
   description: "我的前端知识库",
+  cleanUrls: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
