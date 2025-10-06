1ea75a824ff705b5ee4e6903e8762c40186f452e
BarryDong <286084845@qq.com>
2025-10-06 19:12:28 +0800

    Git 常用操作

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,11 +8,16 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: '知识库', link: '/knowledge/git' },
       { text: '解决方案', link: '/solution/wechat-scan-login' },
       { text: '作品集', link: '/project/fastgen' },
     ],
 
     sidebar: {
+      // 知识库
+      '/knowledge/': [
+        { text: 'Git', link: '/knowledge/git' },
+      ],
       // 解决方案
       '/solution/': [
         { text: '微信扫码登录', link: '/solution/wechat-scan-login' },
