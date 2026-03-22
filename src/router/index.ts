4390d1fc63cba00ca51c6c14dfd37915be6da3a2
BarryDong <286084845@qq.com>
2026-03-22 10:34:46 +0800

    refactor: 切换为 Hash 路由模式以适配 GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import NotFound from '@/views/404/index.vue'
 import Home from '@/views/home/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
